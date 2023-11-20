19ae9c3e6a2d059aa8491bfb8571f95aa4e19f75
postrel63 <postrel63@naver.com>
2023-11-21 00:15:53 +0900

    feat. redisson Lock 구현

---
+++ src/test/java/com/example/stock/facade/RedissonLockStockFacadeTest.java
@@ -0,0 +1,70 @@
+package com.example.stock.facade;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class RedissonLockStockFacadeTest {
+
+
+
+    private final  RedissonLockStockFacade redissonLockStockFacade;
+
+    private final StockRepository stockRepository;
+
+    public RedissonLockStockFacadeTest(RedissonLockStockFacade redissonLockStockFacade, StockRepository stockRepository) {
+        this.redissonLockStockFacade = redissonLockStockFacade;
+        this.stockRepository = stockRepository;
+    }
+
+    @Autowired
+
+
+
+    @BeforeEach
+    public void before() {
+        stockRepository.saveAndFlush(new Stock(1L, 100L));
+    }
+
+    @AfterEach
+    public void after() {
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    public void 동시에_100개의_요청() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() ->
+            {
+                try {
+                    redissonLockStockFacade.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        assertEquals(0, stock.getQuantity());
+
+    }
+
+
+}
